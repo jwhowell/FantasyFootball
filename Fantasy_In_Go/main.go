b3625488fe06e8bdb7818e1b81aa9b44d83bb942
jwhowell <jwh1097@gmail.com>
2025-10-10 21:19:38 -0400

    push

--- Fantasy_In_Go/main.go
+++ Fantasy_In_Go/main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
+type Roster struct {
+	Points         float64            `json:"points"`
+	Players        []string           `json:"players"`
+	RosterID       int                `json:"roster_id"`
+	CustomPoints   *float64           `json:"custom_points"` // nullable
+	MatchupID      int                `json:"matchup_id"`
+	Starters       []string           `json:"starters"`
+	StartersPoints []float64          `json:"starters_points"`
+	PlayersPoints  map[string]float64 `json:"players_points"`
+}
+
 func main() {
-	html, err := fetchHTML("https://api.sleeper.app/v1/league/1258515704311709696/matchups/1")
+	var rosters []Roster
+
+	data, err := fetchHTML("https://api.sleeper.app/v1/league/1258515704311709696/matchups/1")
 	if err != nil {
 		log.Printf("Error at fetchHTML")
 	}
-	fmt.Println(html)
+	err = json.Unmarshal([]byte(data), &rosters)
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Println(rosters[0])
 
+	db, err := sql.Open("sqlite", "football.db?_foreign_keys=1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	ctx := context.Background()
+
+	_, err = db.ExecContext(ctx, `
+		CREATE TABLE IF NOT EXISTS football (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		);
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func fetchHTML(url string) (string, error) {
